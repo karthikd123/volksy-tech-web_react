f2231b76da6c01fd6f2e07bd650340fbe0e66c0b
Your Name <you@example.com>
2023-03-28 05:35:23 +0000

    i5

--- task_0/src/index.js
+++ task_0/src/index.js
@@ -1,6 +1,7 @@
-'use strict';
-const $ = require('jquery');
+// interesting jquery import thanks to babel
+import $ from 'jquery';
 
+// add three different p tags to body
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<p>Copyright - Holberton School</p>');
